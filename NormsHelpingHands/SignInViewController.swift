48ff85fc4c31d0d27577d2fb99a69b9787c3bace
Deb Das <ddas4@uncc.edu>
2020-02-11 12:47:03 -0500

    Added signin VC

---
+++ NormsHelpingHands/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  NormsHelpingHands
+//
+//  Created by Deb Das on 2/11/20.
+//  Copyright © 2020 Deb Das. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
