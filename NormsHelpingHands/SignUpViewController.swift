2238daa917f911de94bac07ded948697e0a9bc41
gvargas1 <georgevcr2@Georges-MBP.attlocal.net>
2020-02-13 00:09:51 -0500

    Added validation to Sign Up and functionality to Create Account button

--- NormsHelpingHands/SignUpViewController.swift
+++ NormsHelpingHands/SignUpViewController.swift
@@ -15,6 +15,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var errorField: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +34,65 @@ class SignUpViewController: UIViewController {
     }
     */
     
-    let confirmEmail = "@uncc.edu"
+    func validateFields() -> String? {
+        //email regex, checking for @uncc.edu (insesntive)
+        let emailRegEx = "[A-Z0-9a-z.-_]+@(?i)(uncc)+\\.(edu)(?-i)"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        var vaildEmail = emailPred.evaluate(with: emailField.text!)
+        
+        //checking for empty field, etc.
+        if firstNameField.text!.trimmingCharacters(in: .whitespacesAndNewlines) == "" || lastNameField.text!.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailField.text!.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordField.text!.trimmingCharacters(in: .whitespacesAndNewlines) == "" || confirmPasswordField.text!.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            !vaildEmail {
+            
+            return "Please use UNCC email and check for empty fields"
+        }
+        
+        if passwordField.text != confirmPasswordField.text {
+            return "Passwords do not match"
+        }
+        
+        return nil
+    }
     
     @IBAction func onCreateAccountClicked(_ sender: UIButton) {
+        //validate fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            //there is something wrong /w fields
+            showError(error!)
+            
+        } else {
+            
+            //create the account
+            
+            
+            
+            //
+            
+            //transition to home screen
+            self.transitionToHome()
+            
+        }
+        
+    }
+    
+    func showError(_ message:String) {
+        errorField.text = message
+        errorField.alpha = 1
+        errorField.sizeToFit()
+    }
+    
+    func transitionToHome() {
+    
+        let tabViewController = storyboard?.instantiateViewController(identifier: "TabVC")
+        
+        view.window?.rootViewController = tabViewController
+        view.window?.makeKeyAndVisible()
+        
     }
     
 }
