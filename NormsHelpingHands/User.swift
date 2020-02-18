4823f74662885554fa7390ca550d6487ac2a17fb
Deb Das <ddas4@uncc.edu>
2020-02-18 11:33:38 -0500

    Added pod file and user class

---
+++ NormsHelpingHands/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  NormsHelpingHands
+//
+//  Created by Deb Das on 2/18/20.
+//  Copyright © 2020 Deb Das. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    var email : String = ""
+    var password : String = ""
+    var confirmPassword : String = ""
+    
+    
+    
+}
